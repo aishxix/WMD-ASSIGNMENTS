6bbbe79e8506ee2e08179287756869fbd86cd2eb
aishxix <156312533+aishxix@users.noreply.github.com>
2024-03-03 12:53:04 +0500

    Add files via upload

---
+++ 2ndassign.js
@@ -0,0 +1,7 @@
+// let famous_person = 'Nelson Mandela'
+// let message = '“The greatest glory in living lies not in never falling, but in rising every time we fall.”'
+// console.log(message)
+var myName = "\t Aish \n";
+console.log("Name with whitespace:", myName);
+var strippedName = 'Aish';
+console.log('Name without whitespace:', strippedName);
---
+++ 2ndassign.ts
@@ -0,0 +1,8 @@
+// let famous_person = 'Nelson Mandela'
+// let message = '“The greatest glory in living lies not in never falling, but in rising every time we fall.”'
+// console.log(message)
+
+let myName: string = "\t Aish \n";
+ console. log("Name with whitespace:", myName); 
+ let strippedName: string = 'Aish'
+ console.log('Name without whitespace:', strippedName)
